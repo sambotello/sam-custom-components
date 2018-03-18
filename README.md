16296054b5d6ad79abf4493529bf3232f26af0da
Sam Botello <sambotello.dev@gmail.com>
2018-03-18 02:55:19 -0600

    Input

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Sam React Custom Components
+
+## Input
+```
+label: [string]
+type: [string]
+name: [string]
+value: [ [func]
+inline: [bool]
+big: [bool]
+dark: [bool]
+```
+
+Example:
+
+```
+<Input label="Input Test" name="val" value={val} onChange={this.onChange} />
+<Input label="Input Test" name="val" value={val} onChange={this.onChange} inline />
+<Input label="Input Test" name="val" value={val} onChange={this.onChange} inline dark/>
+<Input label="Input Test Big" name="val" value={val} onChange={this.onChange} big dark/>
+```
--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 
-import TestComponent from 'components/TestComponent';
+import Input from 'components/Input';
 
 class App extends Component {
+    state = {
+        val: '',
+    }
+
+    onChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
     render() {
+        const { val } = this.state;
         return (
             <div>
                 <h1>Sam Custom Components</h1>
-                <TestComponent />
+                <Input label="Input Test" name="val" value={val} onChange={this.onChange} />
+                <Input label="Input Test" name="val" value={val} onChange={this.onChange} inline />
+                <Input label="Input Test" name="val" value={val} onChange={this.onChange} inline dark/>
+                <Input label="Input Test Big" name="val" value={val} onChange={this.onChange} big dark/>
             </div>
         );
     }
