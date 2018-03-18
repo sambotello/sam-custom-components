3ccc6b80ba879a6b776477a49d71351dd721aa6c
Sam Botello <sambotello.dev@gmail.com>
2018-03-18 01:44:44 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Sam React Custom Components
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import TestComponent from 'components/TestComponent';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Sam Custom Components</h1>
+                <TestComponent />
+            </div>
+        );
+    }
+}
+
+export default App;
